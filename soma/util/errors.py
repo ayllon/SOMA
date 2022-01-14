e90ca4f1ed16eac434ca89d933b745f3e1c7b6f6
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-01-14 21:33:08 +0100

    Prepare notebook for sample size

--- soma/util/errors.py
+++ soma/util/errors.py
@@ -1,6 +1,9 @@
-from typing import Callable, Dict
+from datetime import timedelta
+from time import perf_counter
+from typing import Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
+from pandas import MultiIndex, DataFrame
 from tqdm.auto import tqdm
 
 from soma.generators import Generator
@@ -8,10 +11,12 @@ from soma.generators.kbest import KBestGenerator
 
 
 def compute_errors(gen_a: Generator, gen_b: Generator, test: Callable, *, alpha: float = 0.1, samples: int = 300,
-                   repeat: int = 500):
+                   repeat: int = 500, return_duration: bool = False) \
+        -> Union[Tuple[float, float], Tuple[float, float, Optional[timedelta]]]:
     error1 = 0.
     error2 = 0.
 
+    start = perf_counter()
     for _ in range(repeat):
         a = gen_a.sample(samples)
         a2 = gen_a.sample(samples)
@@ -20,7 +25,11 @@ def compute_errors(gen_a: Generator, gen_b: Generator, test: Callable, *, alpha:
         ph1 = test(a, b)
         error1 += ph0 <= alpha
         error2 += ph1 > alpha
-
+    end = perf_counter()
+    # 2 tests inside the loop!
+    duration = (end - start) / (2. * repeat)
+    if return_duration:
+        return error1 / repeat, error2 / repeat, duration
     return error1 / repeat, error2 / repeat
 
 
@@ -39,3 +48,19 @@ def stat_errors_vs_dimension(gen_a: Generator, gen_b: Generator, tests: Dict[str
             results[test_name][i, :] = compute_errors(kbest_a_gen, kbest_b_gen, test, alpha=alpha, samples=samples,
                                                       repeat=repeat)
     return dimensions, results
+
+
+def stat_errors_vs_sample_size(gen_a: Generator, gen_b: Generator, tests: Dict[str, Callable], samples: np.ndarray, *,
+                               alpha: float = 0.1, repeat: int = 500) -> DataFrame:
+    assert gen_a.dimensions == gen_b.dimensions
+
+    results = DataFrame(columns=['error1', 'error2', 'time'],
+                        index=MultiIndex.from_product([tests.keys(), samples], names=['test', 'samples']))
+    progress = tqdm(total=len(samples) * len(tests))
+    for sample_size in samples:
+        for test_name, test in tests.items():
+            results.loc[test_name, sample_size].loc[:] = compute_errors(gen_a, gen_b, test, alpha=alpha,
+                                                                        samples=sample_size,
+                                                                        repeat=repeat, return_duration=True)
+            progress.update(1)
+    return results
--- soma/util/plot.py
+++ soma/util/plot.py
@@ -1,7 +1,6 @@
-from typing import Dict
-
 import numpy as np
 from matplotlib import pyplot as plt
+from pandas import DataFrame
 
 
 def plot_divergences(dimensions: np.ndarray, divergences: np.ndarray, mean: float, std: float,
@@ -18,19 +17,42 @@ def plot_divergences(dimensions: np.ndarray, divergences: np.ndarray, mean: floa
     return plt.gcf()
 
 
-def plot_power(dimensions: np.ndarray, results: Dict[str, np.ndarray], *, alpha: float = 0.1):
+def plot_errors(results: DataFrame, *, alpha: float = 0.1):
+    tests = results.index.levels[0].values
+    xval = results.index.levels[1].values
+    xlabel = results.index.names[1].capitalize()
+
     figsize = plt.rcParams['figure.figsize']
     fig, axes = plt.subplots(ncols=2, sharex=True, figsize=(figsize[0], figsize[1] / 2))
-    for test_name, test_results in results.items():
-        axes[0].plot(dimensions, test_results[:, 0], label=test_name)
-        axes[1].plot(dimensions, test_results[:, 1], label=test_name)
+
+    for test_name in tests:
+        test_results = results.loc[test_name]
+        axes[0].plot(xval, test_results['error1'], label=test_name)
+        axes[1].plot(xval, test_results['error2'], label=test_name)
     axes[1].legend()
     axes[0].axhline(alpha, linestyle='--', color='red')
-    axes[0].set_xlabel('Dimensions')
+    axes[0].set_xlabel(xlabel)
     axes[0].set_ylabel('$\\alpha$')
-    axes[1].set_xlabel('Dimensions')
+    axes[1].set_xlabel(xlabel)
     axes[1].set_ylabel('$\\beta$')
     axes[0].set_title('Type I Error')
     axes[1].set_title('Type II Error')
-    plt.tight_layout()
+    fig.tight_layout()
+    return fig
+
+
+def plot_time(results: DataFrame):
+    tests = results.index.levels[0].values
+    xval = results.index.levels[1].values
+    xlabel = results.index.names[1].capitalize()
+
+    fig = plt.figure()
+    ax = fig.gca()
+    for test_name in tests:
+        test_results = results.loc[test_name]
+        ax.plot(xval, test_results['time'], label=test_name)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel('Seconds')
+    fig.legend()
+    fig.tight_layout()
     return fig
