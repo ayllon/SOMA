3408b9cefa8ceecae8fb74bca7e32c15f2eba5a9
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-03-22 13:57:46 +0100

    Rosenblatt setup

--- soma/tests/som.py
+++ soma/tests/som.py
@@ -5,9 +5,8 @@ import somoclu
 from scipy.stats import chi2
 
 
-def som_test(a: np.ndarray, b: np.ndarray, size: Tuple[int, int] = (10, 10),
-             ret_som: bool = False,
-             ret_counts: bool = False, **kwargs) -> Union[float, Tuple[float, ...]]:
+def som_statistic(a: np.ndarray, b: np.ndarray, size: Tuple[int, int] = (10, 10), ret_som: bool = False,
+                  ret_counts: bool = False, ret_df: bool = False, **kwargs):
     c = np.concatenate([a, b]).astype(np.float32)
     som = somoclu.Somoclu(size[0], size[1], **kwargs)
     som.train(c)
@@ -19,14 +18,28 @@ def som_test(a: np.ndarray, b: np.ndarray, size: Tuple[int, int] = (10, 10),
     ap = (ap <= ap.min(axis=1)[:, np.newaxis]).sum(axis=0).reshape(size[1], size[0])
     bp = (bp <= bp.min(axis=1)[:, np.newaxis]).sum(axis=0).reshape(size[1], size[0])
     gt0 = (ap + bp) > 0
-    c2 = np.sum(((ap - bp) ** 2)[gt0] / (ap + bp)[gt0])
-
     c = int(len(a) == len(b))
-    ret = [1 - chi2.cdf(c2, df=gt0.sum() - c)]
+    c2 = np.sum(((ap - bp) ** 2)[gt0] / (ap + bp)[gt0])
+    ret = [c2]
     if ret_som:
         ret.append(som)
     if ret_counts:
         ret.append((ap, bp))
+    if ret_df:
+        ret.append(gt0.sum() - c)
+    return ret
+
+
+def som_test(a: np.ndarray, b: np.ndarray, size: Tuple[int, int] = (10, 10),
+             ret_som: bool = False,
+             ret_counts: bool = False, **kwargs) -> Union[float, Tuple[float, ...]]:
+    t, som, counts, df = som_statistic(a, b, size, ret_som=True, ret_counts=True, ret_df=True)
+
+    ret = [1 - chi2.cdf(t, df=df)]
+    if ret_som:
+        ret.append(som)
+    if ret_counts:
+        ret.append(counts)
     if len(ret) > 1:
         return tuple(ret)
     return ret[0]
